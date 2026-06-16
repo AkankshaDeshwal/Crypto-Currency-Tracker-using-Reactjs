5676c833e0354e8d82aea32e209a97f0301df61a
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-16 18:17:08 +0530

    feat:Implement fetch coin daya by id and get data in CoinDetails page

--- src/pages/CoinDetails.jsx
+++ src/pages/CoinDetails.jsx
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchCoinData } from "../api/CoinGecko";
+
 const CoinDetails = () => {
+    const {id} = useParams();
+    const [coinData, setCoinData] = useState({})
+
+    const loadCoinData = async () => {
+        try {
+            const data = await fetchCoinData(id)
+            setCoinData(data)
+        }
+        catch(err){
+            console.error(err)
+        }
+    }
+
+    useEffect(() => {
+        loadCoinData()
+    }, [id])
     return ( <div>This is coint details page.</div> );
 }
  

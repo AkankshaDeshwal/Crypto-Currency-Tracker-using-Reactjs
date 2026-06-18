cb046b3ba90cae2b9fdce2803924032cd6a50d6b
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-18 13:18:24 +0530

    fix:Text change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return ( 
-        <div className="w-full py-4 flex justify-center bg-bg-secondary mt-10">
-            <span className="text-sm">Data Provided by CoinGecko API. Updates every 30 seconds.</span>
+        <div className="w-full py-4 flex justify-center bg-bg-secondary mt-14">
+            <span className="text-sm">Data Provided by CoinGecko API.</span>
         </div>
      );
 }

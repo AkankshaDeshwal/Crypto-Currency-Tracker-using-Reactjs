0666f2f007364f616ddf7f8c9545c1c69e11a880
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-18 12:57:25 +0530

    fix:Truncate long names to keep Crypto cards of same height

--- src/components/CryptoCard.jsx
+++ src/components/CryptoCard.jsx
@@ -5,13 +5,14 @@ import MarketRankPill from "./MarketRankPill";
 import PercentChangeDisplay from "./PercentChangeDisplay";
 
 const CryptoCard = ({crypto}) => {
+    console.log(crypto.name.length)
     return ( 
     <Link to={`/coin/${crypto.id}`}>
     <div className="w-full px-6 py-4 bg-bg-secondary mb-4 rounded-md flex flex-col gap-2 items-start hover:scale-105 cursor-pointer duration-100 ease-in">
         <div className="flex justify-start items-center gap-3">
             <img src={crypto.image} alt={crypto.name} className="w-10" />
             <div className="flex flex-col items-start gap-1">
-                <h1 className="font-semibold">{crypto.name}</h1>
+                <h1 className="font-semibold" >{crypto.name.length>22? (crypto.name.slice(0, 19) + "..."): crypto.name}</h1>
                 <span className="text-xs">{crypto.symbol.toUpperCase()}</span>
                 <MarketRankPill rank={`#${crypto.market_cap_rank}`} additionalStyles='text-sm' />
             </div>

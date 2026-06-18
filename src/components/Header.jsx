73d192d836677934be55ace25f4fe1b2b3ccf76c
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-18 13:17:58 +0530

    chore:Redirect to Home on click of website logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import { ArrowLeft } from 'lucide-react';
 import headerImage from '../assets/images/header-icon.jpg'
 import Button from './Button';
+import { Link } from 'react-router-dom';
 
 const Header = ({sideComp, sideCompHandler}) => {
     return ( 
         <nav className="w-full md:max-w-2/3 h-30 flex justify-between items-center mt-8">
             <div className="flex flex-col items-start gap-2">
+                <Link to='/'>
                 <div className='flex items-center'>
                     <img src={headerImage} alt="crypto icon in header" className=' w-8 md:w-14' />
                     <h1 className="text-3xl">Crypto Tracker</h1>
-                </div>
+                </div></Link>
                 <span className='text-sm'>Real time crypto-currency prices and market data</span>
             </div>
             <div>
